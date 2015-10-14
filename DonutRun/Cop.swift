ed125785e2b94a39aa4649274cd45f1440d6f91c
its-micah <mlanier86@gmail.com>
2015-10-14 18:06:20 -0500

    update Cop physics body

--- DonutRun/Cop.swift
+++ DonutRun/Cop.swift
@@ -33,18 +33,17 @@ class Cop: SKSpriteNode {
 
 
 
-        //let body:SKPhysicsBody = SKPhysicsBody(circleOfRadius: imageTexture.size().width / 2 )
-        let body:SKPhysicsBody = SKPhysicsBody(texture: imageTexture, size: imageTexture.size())
-        //let body:SKPhysicsBody = SKPhysicsBody(circleOfRadius: imageTexture.size().height / 2.5)
+
+        let body:SKPhysicsBody = SKPhysicsBody(circleOfRadius: imageTexture.size().height / 2.6)
 
         body.dynamic = true
         body.affectedByGravity = true
         body.allowsRotation = false
         body.restitution = 0
-        //body.categoryBitMask = BodyType.deathObject.rawValue
+        body.categoryBitMask = BodyType.deathObject.rawValue
         body.contactTestBitMask = BodyType.platformObject.rawValue | BodyType.deathObject.rawValue | BodyType.water.rawValue | BodyType.grass.rawValue
         body.collisionBitMask = BodyType.platformObject.rawValue | BodyType.grass.rawValue
-        body.friction = 0.9 //0 is like glass, 1 is like sandpaper to walk on
+//        body.friction = 0.9 //0 is like glass, 1 is like sandpaper to walk on
         self.physicsBody = body
 
         setUpRun()
