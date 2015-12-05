8cfad951322738c4dc695dfdcf9bd4d74caf3ba9
its-micah <mlanier86@gmail.com>
2015-12-05 10:29:38 -0600

    add SKTexture Extension class

---
+++ DonutRun/SKTexture+Extentions.swift
@@ -0,0 +1,37 @@
+//
+//  SKTexture+Extentions.swift
+//  DonutRun
+//
+//  Created by Mick Lerche on 11/3/15.
+//  Copyright © 2015 Grumpy Dane Studios. All rights reserved.
+//
+
+import SpriteKit
+
+public extension SKTexture {
+
+    public static func createAtlas(atlasNamed: String, numberOfImages: Int) -> Array<SKTexture> {
+        let textureAtlas = SKTextureAtlas(named:atlasNamed + ".atlas")
+
+        var spriteArray = Array<SKTexture>()
+        for var i = 1; i <= numberOfImages; i++ {
+            spriteArray.append(textureAtlas.textureNamed(atlasNamed + "_" + String(i)))
+        }
+
+        return spriteArray
+    }
+
+    public static func createAtlas(atlasNamed: String, imageBatchNumber: Int, numberOfImages: Int) -> Array<SKTexture> {
+        let textureAtlas = SKTextureAtlas(named:atlasNamed + ".atlas")
+
+        var spriteArray = Array<SKTexture>()
+        for var i = 1; i <= numberOfImages; i++ {
+            spriteArray.append(textureAtlas.textureNamed(atlasNamed + String(imageBatchNumber) + "_" + String(i)))
+        }
+
+        return spriteArray
+    }
+}
+
+
+
